2eda4b9f8c9cd81e2cabeced10b49be56882eada
Jinny-Jin <katie9796@naver.com>
2024-01-24 00:45:36 +0900

    예제2 - todo 생성 기능 추가

--- src/pages/practice2/components/TodoContent.tsx
+++ src/pages/practice2/components/TodoContent.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 const TodoContent = () => {
@@ -5,6 +6,10 @@ const TodoContent = () => {
     const content = useSelector((state : any) => state.contents)
     const mode = useSelector((state : any) => state.mode)
     const selectedId = useSelector((state: any)=> state.selectedId)
+    const [inputValue, setInputValue] = useState<any>({
+        title : '',
+        desc : ''
+    })
 
     return(
         <div className='container'>
@@ -22,13 +27,19 @@ const TodoContent = () => {
                 <>
                     <div>
                         <label htmlFor='title'>Title</label>
-                        <input type='text' id='title'/>
+                        <input type='text' id='title' onChange={(e)=>{
+                            setInputValue({...inputValue, title : e.target.value})
+                        }}/>
                     </div>
                     <div className='content'>
-                        <label htmlFor='content'>Detail</label>
-                        <textarea id='content'/>
+                        <label htmlFor='desc'>Detail</label>
+                        <textarea id='desc' onChange={(e)=>{
+                            setInputValue({...inputValue, desc : e.target.value})
+                        }}/>
                     </div>
-                    <button>생성</button>
+                    <button onClick={()=>{
+                        dispatch({type : 'CREATE', title : inputValue.title, desc : inputValue.desc})
+                    }}>생성</button>
                 </>
             
             )}
--- src/pages/practice2/reducer.ts
+++ src/pages/practice2/reducer.ts
@@ -1,4 +1,3 @@
-
 const initialState = {
     contents : [
         {id:1, title : '6시 기상', desc:'6시 칼 기상 후 출근 준비'},
@@ -6,20 +5,26 @@ const initialState = {
         {id:3, title : '블로그 작성', desc : '퇴근 후 오늘 일상 글 작성'}
     ],
     mode : 'read',
-    selectedId : 3
+    selectedId : 1,
 }
 
 const reducer = (state : any = initialState, action :any) => {
-    let newState
     switch (action.type) {
         case 'SELECT':
             return(
-                newState = Object.assign({},state,{selectedId: action.id, mode: 'read'})
-            )
+                {...state, selectedId: action.id, mode: 'read'}
+                )
         case 'CHANGE_MODE' :
             return(
-                newState = Object.assign({},state,{mode : action.mode})
+                {...state, mode : action.mode}
             )
+        case 'CREATE' :
+            const newContents = [...state.contents,{
+                id : state.contents[state.contents.length-1].id + 1, 
+                title : action.title, 
+                desc : action.desc
+            }]
+            return({...state, contents : newContents})
 
         default:
             return state;
